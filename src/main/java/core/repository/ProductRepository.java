3a2bc5ebb67a31e118ba2a47f84f8a555996a3ae
luxlorys <bossbyrcev221@gmail.com>
2022-07-22 00:56:26 +0300

    created tables User and Product

--- src/main/java/core/repository/ProductRepository.java
+++ src/main/java/core/repository/ProductRepository.java
@@ -1,4 +1,18 @@
 package core.repository;
 
+/*
+create table Product (
+    Id           int auto_increment,
+    Product_name varchar(32) not null,
+    Price        int         not null,
+    constraint Product_pk
+        primary key (Id)
+);
+
+create unique index Product_Product_name_uindex
+    on Product (Product_name);
+
+*/
+
 public class ProductRepository {
 }
--- src/main/java/core/repository/UserRepository.java
+++ src/main/java/core/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package core.repository;
 
+/*
+create table User (
+    id              int auto_increment,
+    First_name      VARCHAR(32) not null,
+    Last_name       VARCHAR(32) not null,
+    Amount_of_money int         not null,
+    constraint User_pk
+    primary key (id)
+);
+*/
+
+
 public class UserRepository {
 }
